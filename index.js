b3534236b2b15fcda65d9733a8c4a6d382eb0312
Matteo <m2lovric@gmail.com>
2021-02-07 16:34:20 +0100

    Restructure code and fix prev transition

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-const slider_top = document.querySelector('.carousel_top__slider');
-const slider_bottom = document.querySelector('.carousel_bottom__slider');
+const sliderTop = document.querySelector('.carousel_top__slider');
+const sliderBottom = document.querySelector('.carousel_bottom__slider');
 
 const prev = document.querySelector('.prev')
 const next = document.querySelector('.next')
 
 let direction;
 
+const transitionEnd = (slider) => {
+  slider.addEventListener('transitionend', () => {  
+    slider.classList.remove('transition');
+
+    if (direction === 1) {
+      setTimeout(() => {
+        slider.prepend(slider.lastElementChild);
+        slider.style.transform = 'translateX(0)';
+      });
+    }
+  })
+}
+
+const slide = (slider) => {
+  if (direction === -1) {
+    let slideWidth = slider.firstElementChild.getBoundingClientRect().width;
+    slider.style.transform = `translateX(${slideWidth + 10}px)`;
+    slider.append(slider.firstElementChild);
+
+    setTimeout(() => {
+      slider.classList.add('transition');
+      setTimeout(() => {
+        slider.style.transform = 'translateX(0)';
+      });
+    });
+
+  } else {
+    slider.classList.add('transition');
+
+    let slideWidth = slider.lastElementChild.getBoundingClientRect().width;
+    slider.style.transform = `translateX(${slideWidth + 10}px)`;
+  }
+}
+
 prev.addEventListener('click', () => {
   direction = -1;
-  let width_top = slider_top.firstElementChild.getBoundingClientRect().width;
-  slider_top.style.transform = `translateX(-${width_top - 10}px)`;
-
-  let width_bottom = slider_bottom.firstElementChild.getBoundingClientRect().width;
-  slider_bottom.style.transform = `translateX(-${width_bottom - 10}px)`;
+  slide(sliderTop);
+  slide(sliderBottom);
 })
 
 next.addEventListener('click', () => {
   direction = 1;
-  let width_top = slider_top.lastElementChild.getBoundingClientRect().width;
-  slider_top.style.transform = `translateX(${width_top + 10}px)`;
-
-  let width_bottom = slider_bottom.lastElementChild.getBoundingClientRect().width;
-  slider_bottom.style.transform = `translateX(${width_bottom + 10}px)`;
+  slide(sliderTop);
+  slide(sliderBottom);
 })
 
-slider_top.addEventListener('transitionend', () => {
-  if (direction === -1) {
-    slider_top.append(slider_top.firstElementChild);
-    slider_bottom.append(slider_bottom.firstElementChild);
-  } else {
-    slider_top.prepend(slider_top.lastElementChild);
-    slider_bottom.prepend(slider_bottom.lastElementChild);
-  }
-
-  slider_top.style.transitionDuration = '0s';
-  slider_bottom.style.transitionDuration = '0s';
-
-  slider_top.style.transform = 'translateX(0)';
-  slider_bottom.style.transform = 'translateX(0)';
-
-  setTimeout(() => {
-    slider_top.style.transition = 'all 0.5s ease-in-out'
-    slider_bottom.style.transition = 'all 0.5s ease-in-out'
-  })
-})
+transitionEnd(sliderTop);
+transitionEnd(sliderBottom);
